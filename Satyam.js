8fd948f83188d7b97477f043322d593c85a9bd15
SahilKumar-064738 <sahilkumar064738@gmail.com>
2024-11-14 14:40:08 +0530

    Add files via upload

---
+++ Satyam.js
@@ -0,0 +1,16 @@
+const backToTopBtn = document.getElementById("backToTop");
+
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+};
+
+backToTopBtn.onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
